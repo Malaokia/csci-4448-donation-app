b371c06e21b0be16003222258c81796c58087e8d
wwcao <wwcao@colorado.edu>
2016-11-01 16:49:31 -0600

    add base of form and usermisc

---
+++ nonprofit_donation/src/nonprofit_donation/usermisc/Profile.java
@@ -0,0 +1,7 @@
+package nonprofit_donation.usermisc;
+
+/**
+ * Created by weipeng on 11/1/16.
+ */
+public abstract class Profile {
+}
---
+++ nonprofit_donation/src/nonprofit_donation/usermisc/UserInfo.java
@@ -0,0 +1,11 @@
+package nonprofit_donation.usermisc;
+
+/**
+ * Created by weipeng on 11/1/16.
+ */
+public class UserInfo {
+    private String username;
+    private String password;
+    private String at;
+
+}
