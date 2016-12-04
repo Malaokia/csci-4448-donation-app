bace5ff24a7f553b60ea94e7828b32b69ad44dc3
malaokia <mng.q.lw@hotmail.com>
2016-12-03 21:30:44 -0700

    Added OrgHomePanel

---
+++ nonprofit_donation/src/nonprofit_donation/panel/OrgHomePanel.java
@@ -0,0 +1,14 @@
+package nonprofit_donation.panel;
+
+import javax.swing.*;
+
+/**
+ * Created by mql-intelli on 12/3/16.
+ */
+public class OrgHomePanel {
+    private JPanel panel1;
+    private JButton btnViewHist;
+    private JButton viewHistoryButton;
+    private JButton editProfileButton;
+    private JButton logoutButton;
+}
